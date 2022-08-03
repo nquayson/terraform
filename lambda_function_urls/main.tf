6751ee1e6a39d2e4e3b08a35d6acc520278a0ee1
Nana <31969365+nquayson@users.noreply.github.com>
2022-08-02 21:30:01 -0400

    Update main.tf

--- lambda_function_urls/main.tf
+++ lambda_function_urls/main.tf
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "myfunc" {
   source_code_hash = data.archive_file.zip.output_base64sha256
   function_name    = "myfunc"
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "func.handler"
+  handler          = "func.handler" #filename.handlermethod
   runtime          = "python3.8"
 
 }
